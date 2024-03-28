a8b831e64212623cb7d53661f0acf997d0205069
es-sai-fi <jhormangomez2005@outlook.com>
2024-03-27 20:15:23 -0500

    Se intenta inicializar la primera ventana del proyecto y agregar el ícono... No funciona.

---
+++ src/main/java/org/example/hangedman/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package org.example.hangedman.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeController {
+
+    @FXML
+    void onHandleButtonPlay(ActionEvent event) throws IOException {
+    }
+}
